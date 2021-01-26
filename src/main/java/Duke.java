74d9eb15b18307ec1ba4bc210a511947c905aa64
ZHU Zikun <75139323+zikunz@users.noreply.github.com>
2021-01-26 20:22:47 +0800

    Add Increment: Level-2: Add, List
    This version of Duke has the ability to store whatever text entered by the user and display them back to the user when requested. Since arrayList is used, the limit on the number of the user inputs depends on the computer memory.

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,3 +1,4 @@
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Duke {
@@ -9,22 +10,59 @@ public class Duke {
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
         */
+        ArrayList < String > texts = new ArrayList < String > ();
+
+        greet();
+        Scanner myObj = new Scanner(System.in);
+        String userInput = myObj.nextLine();
+        texts.add(userInput);
+
+        while (true) {
+            if (userInput.equals("bye")) {
+                exit();
+                System.exit(0);
+            } else if (userInput.equals("list")) {
+                printSeparationLine();
+                for (int i = 0; i < texts.size(); ++i) {
+                    System.out.println("     " + (i + 1) + ". " + texts.get(i));
+                }
+                printSeparationLine();
+                userInput = myObj.nextLine();
+                texts.add(userInput);
+            } else {
+                echo(userInput);
+                userInput = myObj.nextLine();
+                if (!userInput.equals("list")) {
+                    texts.add(userInput);
+                }
+            }
+        }
+    }
+
+    static void greet() {
         String greet = "    ____________________________________________________________\n" +
                 "     Hello! I'm Duke\n" +
                 "     What can I do for you?\n" +
                 "    ____________________________________________________________\n";
+        System.out.println(greet);
+    }
+
+    static void printSeparationLine() {
+        System.out.println("    ____________________________________________________________");
+    }
+
+    static void echo(String userInput) {
+        printSeparationLine();
+        System.out.println("     added: " + userInput);
+        printSeparationLine();
+        System.out.println();
+    }
+
+    static void exit() {
         String exit = "    ____________________________________________________________\n" +
                 "     Bye. Hope to see you again soon!\n" +
                 "    ____________________________________________________________";
-        String line = "    ____________________________________________________________\n";
-
-        System.out.println(greet);
-        Scanner myObj = new Scanner(System.in);
-        String userInput = myObj.nextLine();
-        while (!userInput.equals("bye")) {
-            System.out.println(line + "     " + userInput + "\n" + line);
-            userInput = myObj.nextLine();
-        }
         System.out.println(exit);
     }
+
 }
