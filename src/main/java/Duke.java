dc909a760ea20f08b9dbc37c9d4179ab57445585
ZHU Zikun <75139323+zikunz@users.noreply.github.com>
2021-01-26 19:37:02 +0800

    Add Increment: Level-1: Greet, Echo, Exit
    This is a skeletal version of Duke that starts by greeting the user, simply echoes commands entered by the user, and exits when the user types "bye".

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
         /*
@@ -7,12 +9,22 @@ public class Duke {
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
         */
-        String greet = "____________________________________________________________\n" +
-                " Hello! I'm Duke\n" +
-                " What can I do for you?\n" +
-                "____________________________________________________________\n" +
-                " Bye. Hope to see you again soon!\n" +
-                "____________________________________________________________";
+        String greet = "    ____________________________________________________________\n" +
+                "     Hello! I'm Duke\n" +
+                "     What can I do for you?\n" +
+                "    ____________________________________________________________\n";
+        String exit = "    ____________________________________________________________\n" +
+                "     Bye. Hope to see you again soon!\n" +
+                "    ____________________________________________________________";
+        String line = "    ____________________________________________________________\n";
+
         System.out.println(greet);
+        Scanner myObj = new Scanner(System.in);
+        String userInput = myObj.nextLine();
+        while (!userInput.equals("bye")) {
+            System.out.println(line + "     " + userInput + "\n" + line);
+            userInput = myObj.nextLine();
+        }
+        System.out.println(exit);
     }
 }
