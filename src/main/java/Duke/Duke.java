ba01e73f95559db9a5406108b4aebe19ad740ced
Zhu Zikun @ NUS <zhu.zikun@u.nus.edu>
2021-02-10 23:37:33 +0800

    Add Increment: Level-5: Handle Errors
    This version of Duke deals with errors such as incorrect inputs entered
    by the user.

--- src/main/java/Duke/Duke.java
+++ src/main/java/Duke/Duke.java
@@ -21,7 +21,12 @@ public class Duke {
     private static String EXIT_MESSAGE = SEPARATION_LINE + "\n" + "     Bye. Hope to see you again soon!\n" +
             SEPARATION_LINE;
 
-    private static char NOT_DONE_SYMBOL = '\u2718';
+    private static String NOT_DONE_SYMBOL = "\u2718";
+
+    //public static int WRONG_COMMAND_ERROR = 0;
+    //private static int TODO_ERROR = 1;
+
+
 
 
     public static void main(String[] args) {
@@ -43,62 +48,112 @@ public class Duke {
         }
 
         while (true) {
-            if (userInput.startsWith("todo ")) {
-                Task newTask = new Todo(userInput.substring(5));
-                addTask(newTask);
-            } else if (userInput.startsWith("deadline ")) {
-                Task newTask = new Deadline(userInput.substring(9, userInput.indexOf("/")),
-                        userInput.substring(userInput.indexOf("/by") + 4));
-                addTask(newTask);
-            } else if (userInput.startsWith("event ")) {
-                Task newTask = new Event(userInput.substring(6, userInput.indexOf("/")),
-                        userInput.substring(userInput.indexOf("/at") + 4));
-                addTask(newTask);
-            } else if (userInput.startsWith("done ")) {
-                int selectedTask = Integer.parseInt(userInput.substring(5)) - 1;
-                tasks[selectedTask].setAsDone();
-                printSeparationLine();
-                System.out.println("     Nice! I've marked this task as done:");
-                System.out.println("       [" + tasks[selectedTask].getStatusIcon() + "] " +
-                        tasks[selectedTask].getDescription());
-                printSeparationLine();
-            } else if (userInput.equals("bye")) {
-                exit();
-                System.exit(0);
-            } else if (userInput.equals("list")) {
-                printSeparationLine();
-                if (numberOfTasks == 1) {
-                    System.out.println("     Here is the task in your list:");
-                } else {
-                    System.out.println("     Here are the tasks in your list:");
-                }
-                for (int i = 0; i < numberOfTasks; i++) {
-                    System.out.println("     " + (i + 1) + "." + tasks[i]);
-                }
-                printSeparationLine();
-                System.out.println();
+            try {
+                processCommand(userInput);
+            } catch(DukeException e) {
+                System.out.println(e.getMessage());
+            } finally {
+                userInput = in.nextLine();
             }
-            userInput = in .nextLine();
+
         }
     }
 
+    static void processCommand(String userInput) throws DukeException {
+        if (userInput.startsWith("todo")) {
+            try {
+                addTodo(userInput);
+            } catch (DukeException e){
+                System.out.println(e.getMessage());
+            }
+        } else if (userInput.startsWith("deadline ")) {
+            addDeadline(userInput);
+        } else if (userInput.startsWith("event ")) {
+            addEvent(userInput);
+        } else if (userInput.startsWith("done ")) {
+            setTaskToBeDone(userInput);
+        } else if (userInput.equals("bye")) {
+            exit();
+        } else if (userInput.equals("list")) {
+            addList();
+        }
+        else {
+            throw new DukeException("     ☹ OOPS!!! I'm sorry, but I don't know what that means :-(");
+        }
+
+    }
+
+    static void addTodo(String userInput) throws DukeException {
+        String todo = userInput.substring(4);
+        todo = todo.replace(" ", "");
+
+        if (todo.isEmpty()) {
+            throw new DukeException("     ☹ OOPS!!! The description of a todo cannot be empty.");
+        }
+        
+        else {
+            Task newTask = new Todo(userInput.substring(5));
+            addTask(newTask);
+        }
+    }
+
+    static void addDeadline(String userInput) {
+            Task newTask = new Deadline(userInput.substring(9, userInput.indexOf("/")),
+                    userInput.substring(userInput.indexOf("/by") + 4));
+            addTask(newTask);
+    }
+
+    static void setTaskToBeDone(String userInput) {
+        int selectedTask = Integer.parseInt(userInput.substring(5)) - 1;
+        tasks[selectedTask].setAsDone();
+        printSeparationLine();
+        System.out.println("     Nice! I've marked this task as done:");
+        System.out.println("       [" + tasks[selectedTask].getStatusIcon() + "] " +
+                tasks[selectedTask].getDescription());
+        printSeparationLine();
+    }
+
+    static void addList() {
+        printSeparationLine();
+        if (numberOfTasks == 1) {
+            System.out.println("     Here is the task in your list:");
+        } else {
+            System.out.println("     Here are the tasks in your list:");
+        }
+        for (int i = 0; i < numberOfTasks; i++) {
+            System.out.println("     " + (i + 1) + "." + tasks[i]);
+        }
+        printSeparationLine();
+        System.out.println();
+    }
+
+    static void addEvent(String userInput) {
+        Task newTask = new Event(userInput.substring(6, userInput.indexOf("/")),
+                userInput.substring(userInput.indexOf("/at") + 4));
+        addTask(newTask);
+    }
+
+    /*static void printErrorMessage(int errorNumber) {
+        if (errorNumber == WRONG_COMMAND_ERROR) {
+            System.out.println("     ☹ OOPS!!! I'm sorry, but I don't know what that means :-(");
+        } else if (errorNumber == TODO_ERROR) {
+            System.out.println("     ☹ OOPS!!! The description of a todo cannot be empty.");
+        }
+    }*/
 
     static void greet() {
         System.out.println(GREET_MESSAGE);
     }
 
-
     static void printSeparationLine() {
         System.out.println(SEPARATION_LINE);
     }
-
 
     public static void addTask(Task t) {
         tasks[numberOfTasks] = t;
         numberOfTasks++;
         echo(t);
     }
-
 
     static void echo(Task newTask) {
         printSeparationLine();
@@ -116,8 +171,8 @@ public class Duke {
         System.out.println();
     }
 
-
     static void exit() {
         System.out.println(EXIT_MESSAGE);
+        System.exit(0);
     }
 }
--- src/main/java/Duke/Event.java
+++ src/main/java/Duke/Event.java
@@ -3,23 +3,19 @@ package Duke;
 public class Event extends Task {
     protected String period;
 
-
     public Event(String description, String period) {
         super(description);
         this.period = period;
     }
 
-
     public String getPeriod() {
         return period;
     }
-
 
     @Override
     public String getType() {
         return "E";
     }
-
 
     @Override
     public String toString() {
